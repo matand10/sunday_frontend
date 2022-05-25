2ad289caf044a156ae9fc819815762098b8e960f
Yael <julu3080@gmail.com>
2022-05-25 23:44:24 +0300

    begining delete

--- src/cmps/group-list.jsx
+++ src/cmps/group-list.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { utilService } from "../services/util.service";
+import { FaChevronCircleDown, FaCaretDown } from 'react-icons/fa'
 
 
 export const GroupList = ({ group, onAddTask }) => {
@@ -19,6 +20,7 @@ export const GroupList = ({ group, onAddTask }) => {
     return <div className="group">
         {/* <h1>{group.title}</h1> */}
         <div className="head">
+            <div className="group-arrow-div"><FaChevronCircleDown className="group-arrow"/></div>
             <div>{group.title}</div>
             <div>Person</div>
             <div>Status</div>
@@ -26,10 +28,11 @@ export const GroupList = ({ group, onAddTask }) => {
         </div>
         {group.tasks.map((task, idx) => {
             return <section key={idx} className="group-row">
-                    <div>{task.title}</div>
-                    <div>{task.assignedTo.map(member => member.fullname + ' ')}</div>
-                    <div>{task.status}</div>
-                    <div>{task.archivedAt ? utilService.getCurrTime(task.archivedAt) : ''}</div>
+                <div className="task-arrow-div"><FaCaretDown className="task-arrow"/></div>
+                <div>{task.title}</div>
+                <div>{task.assignedTo.map(member => member.fullname + ' ')}</div>
+                <div>{task.status}</div>
+                <div>{task.archivedAt ? utilService.getCurrTime(task.archivedAt) : ''}</div>
             </section>
         })}
         <div>
